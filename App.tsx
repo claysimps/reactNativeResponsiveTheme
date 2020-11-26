ffb420fd7401cfa7d6d997725ea22a60d1cd8ee7
claysimps <43582151+claysimps@users.noreply.github.com>
2020-11-26 09:49:17 +0000

    minor fixes

--- App.tsx
+++ App.tsx
@@ -2,16 +2,12 @@ import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
 import {ThemeManager} from './components/ThemeManager';
 
-const App = () => {
-  return (
-    <>
-      <ThemeManager>
-        <SafeAreaView>
-          <Text>Hello world</Text>
-        </SafeAreaView>
-      </ThemeManager>
-    </>
-  );
-};
+const App = () => (
+  <ThemeManager>
+    <SafeAreaView>
+      <Text>Hello world</Text>
+    </SafeAreaView>
+  </ThemeManager>
+);
 
 export default App;
--- components/ThemeManager/ThemeManager.tsx
+++ components/ThemeManager/ThemeManager.tsx
@@ -1,8 +1,8 @@
-import React, {FC} from 'react';
+import React from 'react';
 import {StatusBar} from 'react-native';
 import styled, {ThemeProvider} from 'styled-components/native';
 
-import {CustomThemeProps, light} from '../../constants/Theme';
+import {CustomThemeProps, light} from '../../constants/theme';
 
 const StyledThemeContainer = styled.KeyboardAvoidingView<CustomThemeProps>`
   flex: 1;
@@ -11,7 +11,7 @@ const StyledThemeContainer = styled.KeyboardAvoidingView<CustomThemeProps>`
   background: ${(props) => props.theme.background};
 `;
 
-export const ThemeManager: FC = ({children}) => {
+export const ThemeManager = ({children}: {children: React.ReactNode}) => {
   return (
     <ThemeProvider theme={light}>
       <StatusBar barStyle={'light-content'} />
